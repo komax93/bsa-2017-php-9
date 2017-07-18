9238fcf27d12c30639226488c33c0d42e8d8cb1b
Maxym Kolesnykov <maxkolesnikov93@gmail.com>
2017-07-18 17:25:21 +0300

    initial commit

--- app/Manager/CarManager.php
+++ app/Manager/CarManager.php
@@ -3,9 +3,36 @@
 namespace App\Manager;
 
 
+use App\Entity\Car;
 use App\Manager\Contract\CarManager as CarManagerContract;
+use App\Request\Contract\SaveCarRequest;
+use Illuminate\Support\Collection;
 
 class CarManager implements CarManagerContract
 {
+    public function findAll(): Collection
+    {
+        // TODO: Implement findAll() method.
+    }
+
+    public function findById(int $id)
+    {
+        // TODO: Implement findById() method.
+    }
+
+    public function findCarsFromActiveUsers(): Collection
+    {
+        // TODO: Implement findCarsFromActiveUsers() method.
+    }
+
+    public function saveCar(SaveCarRequest $request): Car
+    {
+        // TODO: Implement saveCar() method.
+    }
+
+    public function deleteCar(int $carId)
+    {
+        // TODO: Implement deleteCar() method.
+    }
 
 }
--- app/Manager/UserManager.php
+++ app/Manager/UserManager.php
@@ -3,9 +3,36 @@
 namespace App\Manager;
 
 
+use App\Entity\User;
 use App\Manager\Contract\UserManager as UserManagerContract;
+use App\Request\Contract\SaveUserRequest;
+use Illuminate\Support\Collection;
 
 class UserManager implements UserManagerContract
 {
+    public function findAll(): Collection
+    {
+        // TODO: Implement findAll() method.
+    }
+
+    public function findById(int $id)
+    {
+        // TODO: Implement findById() method.
+    }
+
+    public function findActive(): Collection
+    {
+        // TODO: Implement findActive() method.
+    }
+
+    public function saveUser(SaveUserRequest $request): User
+    {
+        // TODO: Implement saveUser() method.
+    }
+
+    public function deleteUser(int $userId)
+    {
+        // TODO: Implement deleteUser() method.
+    }
 
 }
