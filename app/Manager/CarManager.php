5ddab19059c6bab9b54ff6ccb42f9725e86ff4c5
Maksym Kolesnykov <maxkolesnikov93@gmail.com>
2017-07-18 23:31:24 +0300

    added comments to CarManager, UserManager

--- app/Manager/CarManager.php
+++ app/Manager/CarManager.php
@@ -10,29 +10,56 @@ use Illuminate\Support\Collection;
 
 class CarManager implements CarManagerContract
 {
+    /**
+     * Find all Cars
+     *
+     * @return Collection
+     */
     public function findAll(): Collection
     {
         // TODO: Implement findAll() method.
     }
 
+    /**
+     * Find Car by ID
+     *
+     * @param int $id
+     * @return Car|null
+     */
     public function findById(int $id)
     {
         // TODO: Implement findById() method.
     }
 
+    /**
+     * Find Cars that belongs only to active users
+     *
+     * @return Collection
+     */
     public function findCarsFromActiveUsers(): Collection
     {
         // TODO: Implement findCarsFromActiveUsers() method.
     }
 
+    /**
+     * Create or Update Car
+     *
+     * @param SaveCarRequest $request
+     * @return Car
+     */
     public function saveCar(SaveCarRequest $request): Car
     {
         // TODO: Implement saveCar() method.
     }
 
+    /**
+     * Delete Car by ID
+     *
+     * @param int $carId
+     * @return void
+     */
     public function deleteCar(int $carId)
     {
         // TODO: Implement deleteCar() method.
     }
-
 }
--- app/Manager/UserManager.php
+++ app/Manager/UserManager.php
@@ -10,26 +10,54 @@ use Illuminate\Support\Collection;
 
 class UserManager implements UserManagerContract
 {
+    /**
+     * Find All Users
+     *
+     * @return Collection
+     */
     public function findAll(): Collection
     {
         // TODO: Implement findAll() method.
     }
 
+    /**
+     * Get User model by ID
+     *
+     * @param int $id
+     * @return User|null
+     */
     public function findById(int $id)
     {
         // TODO: Implement findById() method.
     }
 
+    /**
+     * Find Users that has is_active flag true
+     *
+     * @return Collection
+     */
     public function findActive(): Collection
     {
         // TODO: Implement findActive() method.
     }
 
+    /**
+     * Create or Update User
+     *
+     * @param SaveUserRequest $request
+     * @return User
+     */
     public function saveUser(SaveUserRequest $request): User
     {
         // TODO: Implement saveUser() method.
     }
 
+    /**
+     * Delete User By ID
+     *
+     * @param int $userId
+     * @return void
+     */
     public function deleteUser(int $userId)
     {
         // TODO: Implement deleteUser() method.
