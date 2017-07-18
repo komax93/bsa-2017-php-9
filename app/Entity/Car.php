03e62289a65d56893c29c63802042e99c52b076a
Maksym Kolesnykov <maxkolesnikov93@gmail.com>
2017-07-19 01:18:46 +0300

    resolved first task

--- app/Entity/Car.php
+++ app/Entity/Car.php
@@ -2,8 +2,36 @@
 
 namespace App\Entity;
 
-
 class Car
 {
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'cars';
 
+    /**
+     * These fields can be filled by user
+     *
+     * @var array
+     */
+    protected $fillable = ['color', 'model', 'registration_number', 'year', 'mileage', 'price', 'user_id'];
+
+    /**
+     * These fields can't be filled by user
+     *
+     * @var array
+     */
+    protected $guarded = ['id'];
+
+    /**
+     * This method return belongsTo relationship from cars to users table
+     *
+     * @return mixed
+     */
+    public function user()
+    {
+        return $this->belongTo('App\Entity\User');
+    }
 }
--- app/Entity/User.php
+++ app/Entity/User.php
@@ -2,8 +2,45 @@
 
 namespace App\Entity;
 
-
 class User
 {
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
 
+    /**
+     * These fields can be filled by user
+     *
+     * @var array
+     */
+    protected $fillable = ['first_name', 'last_name', 'is_active'];
+
+    /**
+     * These fields can't be filled by user
+     *
+     * @var array
+     */
+    protected $guarded = ['id'];
+
+    /**
+     * Convert is_active field to boolean type
+     *
+     * @var array
+     */
+    protected $casts = [
+        'is_active' => 'boolean'
+    ];
+
+    /**
+     * This method return hasMany relationship from users to cars table
+     *
+     * @return mixed
+     */
+    public function car()
+    {
+        return $this->hasMany('App\Entity\Car');
+    }
 }
